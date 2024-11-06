82af0641e8c7d0d8f427dddebdd946f591ca7bf1
Pradipta Karmakar <pradiptakarmakar.dev@gmail.com>
2024-11-06 13:00:15 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,4 @@
 salary = float(input ("Enter your salary: "))
 bonus = salary*0.12 if salary<10000 else 0
 bonus += salary * 0.05 if input("male/female: ").lower() == "male" else salary*0.10
-
 print(f" Salary: {salary+bonus}\n Bonus: {bonus}")
